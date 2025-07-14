b98c1ca53b4898141d763d38c53b8fef555c9967
nethmakannangara <nethmakannangara007@gmail.com>
2025-07-14 23:04:51 +0530

    add circles to graphics UI

--- Connect 4/level01.cpp
+++ Connect 4/level01.cpp
@@ -1,8 +1,27 @@
 #include <graphics.h>
 #include "level_1.h"
-
+#include <iostream>
+using namespace std;
 void level01(){
 	initwindow(550,550,"Conncet 4");
+	
+	int x,y;
+	
+	for(int i = 1; i < 6; i++){
+		for(int j = 1; j < 6; j++){
+			circle(i*60,j*60,12);
+		}
+	}
+	
+	while(ismouseclick(WM_LBUTTONDOWN)){
+//		cout << "mouse click" << endl;
+	}
+	
+	getmouseclick(WM_LBUTTONDOWN,x,y);
+	
+	char coords[50];
+    sprintf(coords, "Clicked at: (%d, %d)", x, y);
+    outtextxy(100, 100, coords);
 	
 	getch();
 	closegraph();
